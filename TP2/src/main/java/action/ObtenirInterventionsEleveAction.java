61758c9e3c5381747e6509063f88145278b78a4a
Yikang Su <ysu@insa-lyon.fr>
2023-05-23 15:50:49 +0200

    Added projects

---
+++ TP2/src/main/java/action/ObtenirInterventionsEleveAction.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package action;
+
+import java.util.List;
+import java.util.Objects;
+import metier.modele.Eleve;
+import metier.modele.Intervention;
+import static metier.modele.Intervention_.eleve;
+import metier.service.ServiceClient;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+
+/**
+ *
+ * @author acremona
+ */
+public class ObtenirInterventionsEleveAction extends Action{
+    public void executer(HttpServletRequest request) {
+        System.out.println("appel de ObtenirInterventionsEleveAction");
+        Long idEleveRequest = Long.valueOf((String)request.getParameter("idEleve")) ;
+        HttpSession session = request.getSession(true);
+        Long idEleveSession = (Long) session.getAttribute("idEleve");
+        
+        List<Intervention> interventions = null;
+        if(idEleveSession != null && Objects.equals(idEleveRequest, idEleveSession)){
+            ServiceClient service = new ServiceClient();
+            Eleve eleve = service.trouverEleveParId(idEleveSession);
+            interventions = service.listerInterventionsPourEleve(eleve); 
+        } 
+        else{
+            System.out.println("Eleve pas connecté");
+        }
+        request.setAttribute("interventions", interventions);       
+    }
+}
