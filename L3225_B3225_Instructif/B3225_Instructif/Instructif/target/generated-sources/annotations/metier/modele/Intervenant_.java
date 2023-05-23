5d6fb88575aae4c93bf671f83d8febeb6e16b037
Antoine Cremona <acremona@insa-lyon.fr>
2023-05-23 18:47:08 +0200

    necessary

--- L3225_B3225_Instructif/B3225_Instructif/Instructif/target/generated-sources/annotations/metier/modele/Intervenant_.java
+++ L3225_B3225_Instructif/B3225_Instructif/Instructif/target/generated-sources/annotations/metier/modele/Intervenant_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import metier.modele.Niveau;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-05-23T16:07:18")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-05-23T18:41:40")
 @StaticMetamodel(Intervenant.class)
 public abstract class Intervenant_ { 
 
